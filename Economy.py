489c94f02a8edfff3f73c218e2aa9b62bb5b09fa
Mitch Tester <mitch1080@hotmail.co.uk>
2021-04-16 15:42:25 +0100

    Fix sfx list extending

--- Economy.py
+++ Economy.py
@@ -40,6 +40,7 @@ class EconomyDatabase():
         pass
 
     def GetSFXList(self):
+        self.prettySfxList = ""
         for sfx in self.ProductData:
             self.prettySfxList = self.prettySfxList + sfx + ': ' + \
                 str(self.ProductData[sfx]['currentValue']) + '\n'
